60938d659201230aabfe892ec96254ebd93b9fe3
dubr <dubr.cola@gmail.com>
2016-10-28 16:58:56 +0300

    fix livesearch conditions

--- Section/cfg.php
+++ Section/cfg.php
@@ -22,60 +22,32 @@ return array(
                 '!submenu' => 'none'
             ),
             'settings' => array(
-                /*
-            	'submenu' => array(
-                    'name' => 'submenu',
-                    'label' => fx::alang('Subsections','component_section'),
-                    'type' => 'select',
-                    'values' => array(
-                        'none' => fx::alang('Don\'t show','component_section'),
-                        'active' => fx::alang('Show for the active item','component_section'),
-                        'all' => fx::alang('Show for all items','component_section')
-                    )
-                ),
-                */
                 'extra_infoblocks' => array(
                     'name' => 'extra_infoblocks',
                     'label' => 'Добавить ссылки из другого блока',
                     'type' => 'livesearch',
                     'is_multiple' => true, 
                     'ajax_preload' => true,
-                    'params' => array(
-                        'content_type' => 'infoblock',
-                        'conditions' => array(
-                            'controller' => array(
-                                fx::data('component', 'page')->getAllVariants()->getValues(function($ch) {
-                                    return $ch['keyword'];
-                                }),
-                                'IN'
-                            ),
-                            'id' => array(
-                                $this->getParam('infoblock_id'),
-                                '!='
-                            ),
-                            'site_id' => fx::env('site_id'),
-                            'action' => 'list_infoblock'
-                        )
-                    ),
+                    //'params' => array(
+                    'content_type' => 'infoblock',
+                    'conditions' => array(
+                        'controller' => array(
+                            fx::data('component', 'page')->getAllVariants()->getValues(function($ch) {
+                                return $ch['keyword'];
+                            }),
+                            'IN'
+                        ),
+                        'id' => array(
+                            $this->getParam('infoblock_id'),
+                            '!='
+                        ),
+                        'site_id' => fx::env('site_id'),
+                        'action' => 'list_infoblock'
+                    )
+                    //),
                 )
             ),
         ),
-        /*
-        '*list_submenu*' => array(
-            'name' => 'Submenu',
-            'icon_extra' => 'sub',
-            'settings' => array(
-                'source_infoblock_id' => array(
-                    'label' => fx::alang('Source infoblock','component_section'),
-                    'type' => 'select',
-                    'values' => $source_ibs,
-                ),
-            ),
-            'check_context' => function() {
-                return count(fx::env('page')->getParentIds()) > 0;
-            }
-        ),
-        */
         'list_infoblock' => array(
             'name' => 'Меню',
             'default_scope' => function() {
